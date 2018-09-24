c16e06673024ed5c42ca9a030804394a95ec4d7c
ArieBennett <43255209+ArieBennett@users.noreply.github.com>
2018-09-24 10:29:49 +0930

    Format standizing

--- dog.h
+++ dog.h
@@ -1,16 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
-#include "animal.h"
 
+#include "animal.h"
 #include <string>
 
 class dog : public animal{
+	//*States*
 	std::string eats;
-
-public:
-	dog();
-	std::string makeSound(int repeats);
-	~dog();
+	//*Behaviours*
+	public:
+		dog();
+		std::string makeSound(int repeats);
+		~dog();
 };
 
 #endif
