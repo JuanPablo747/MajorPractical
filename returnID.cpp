2ad5fb9efb8fdc2b999c1db0e8ed5ca1a01024d7
ArieBennett <43255209+ArieBennett@users.noreply.github.com>
2018-09-24 10:45:10 +0930

    Start of commenting ; see lines 8 & 9
    (1) I think this file will need comments.
    - Its very different than our objects.
    - If I couldn't understand what each part did from a quick look over, it probably needs comments.
    (2) Removed the 'magic number' 4 and replaced with making number of words a 'const' to allow use of a variable for the array. REVERT this change if this is not the intention.

--- returnID.cpp
+++ returnID.cpp
@@ -5,32 +5,40 @@ using namespace std;
 int returnID(string word)
 {
     int len = word.length();
-    int noOfAnimals = 4;
-    string animals[4] = {"dog", "cat", "mouse", "pass"};
+    const int noOfAnimals = 4;
+    string animals[noOfAnimals] = {"dog", "cat", "mouse", "pass"};
     int counter;
     int counterTwo = 1;
+
+    // for the number of eligible words...
     for (int k = 0; k < noOfAnimals; k++)
     {
         counter = 0;
+
+        // ???
         if (len == animals[k].length())
         {
+            // for each character in the word...
             for (int i = 0; i < len; i++)
             {
-                //if any letter in the word entered is uppercase, change it to lowercase
+                // if any letter in the word entered is uppercase, change it to lowercase
                 if (isupper(word[i]))
                 {
                     word[i] = tolower(word[i]);
                 }
+                // ???
                 if (word[i] != animals[k][i])
                 {
                     counter++;
                 }
             }
+            // ???
             if (counter == 0)
             {
                 return counterTwo;
             }
         }
+        // ???
         counterTwo++;
     }
     return 99;
