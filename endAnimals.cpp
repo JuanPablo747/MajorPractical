bc74ce86c5b47572cd53a96120fcd9255c8b07e8
sunit <coolsunit10@gmail.com>
2018-09-28 16:13:10 +0930

    adding endAnimals function

---
+++ endAnimals.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;	
+
+extern void pause(int dur);
+void animals()
+{	
+	system("clear");
+	cout << "A cat.." << endl << endl;
+	cout << "      ╱╲    ╱╲  ╭━╮" << endl; 
+	cout << "     ╱╱╲╲__╱╱╲╲ ╰╮┃" << endl;
+	cout << "     ▏┏┳╮ ╭┳┓ ▕  ┃┃" << endl;
+	cout << "     ▏╰┻┛▼┗┻╯ ▕  ┃┃" << endl;
+	cout << "     ╲  ╰┻╯   ╱▔▔ ┃" << endl;
+	cout << "      ╰━┳━━━ ╯    ┃" << endl;
+ 	cout << "        ┃┏┓┣━━┳┳┓┃" << endl;
+	cout << "        ┗┛┗┛  ┗┛┗┛" << endl;
+	pause(1);
+	system("clear");
+	cout << "A cat.." << endl << endl;
+	cout << "      ╱╲    ╱╲  ╭━╮" << endl; 
+	cout << "     ╱╱╲╲__╱╱╲╲ ╰╮┃" << endl;
+	cout << "     ▏┏┳╮ ╭┳┓ ▕  ┃┃" << endl;
+	cout << "  _  ▏╰┻┛▼┗┻╯ ▕  ┃┃" << endl;
+	cout << "     ╲  ╰┻╯   ╱▔▔ ┃" << endl;
+	cout << "  /  |╰━┳━━━ ╯    ┃" << endl;
+ 	cout << " MEOW   ┃┏┓┣━━┳┳┓┃" << endl;
+	cout << "        ┗┛┗┛  ┗┛┗┛" << endl;
+	pause(1);
+	system("clear");
+	cout << "A dog.." << endl << endl;
+	cout << "    ╱▏      ▕╲▕╲" << endl;
+	cout << "    ▏▏      ▕▏▔▔╲" << endl;
+	cout << "    ▏╲      ╱ ▔ ▔╲" << endl;
+	cout << "    ╲▏▔▔▔▔▔▔╯╯╰┳━━▀" << endl;
+	cout << "     ▏╯╯╯╯╯╯╯╯╱┃" << endl;
+	cout << "     ┃┏┳┳━━━┫┣┳┃" << endl;
+	cout << "     ┃┃┃┃   ┃┃┃┃" << endl;
+	cout << "     ┗┛┗┛   ┗┛┗┛" << endl;
+	pause(1);
+	system("clear");
+	cout << "A dog.." << endl << endl;
+	cout << "    ╱▏      ▕╲▕╲" << endl;
+	cout << "    ▏▏      ▕▏▔▔╲" << endl;
+	cout << "    ▏╲      ╱ ▔ ▔╲" << endl;
+	cout << "    ╲▏▔▔▔▔▔▔╯╯╰┳━━▀" << endl;
+	cout << "     ▏╯╯╯╯╯╯╯╯╱┃ / | \\" << endl;
+	cout << "     ┃┏┳┳━━━┫┣┳┃  Woof" << endl;
+	cout << "     ┃┃┃┃   ┃┃┃┃" << endl;
+	cout << "     ┗┛┗┛   ┗┛┗┛" << endl;
+	pause(1);
+	system("clear");
+	cout << "A mouse.." << endl << endl;
+	cout << "                    ___" << endl;
+	cout << "           _  _  .-'   '-." << endl;
+	cout << "          (.)(.)/         \\  " << endl;
+	cout << "           /@@             ;          /   " << endl;
+	cout << "          o_\\\\-mm-......-mm`~~~~~~~~~' " << endl;
+	pause(1);
+	system("clear");
+	cout << "A mouse.." << endl << endl;
+	cout << "                    ___" << endl;
+	cout << "           _  _  .-'   '-." << endl;
+	cout << "          (.)(.)/         \\  " << endl;
+	cout << "skrrr  _\\  /@@             ;          /   " << endl;
+	cout << "        / o_\\\\-mm-......-mm`~~~~~~~~~' " << endl;
+	pause(1);
+}
