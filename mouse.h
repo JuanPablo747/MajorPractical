36cf1d986b8ff522887dbaf7c7cc460d44018893
ArieBennett <43255209+ArieBennett@users.noreply.github.com>
2018-10-11 10:43:10 +1030

    comments

--- mouse.h
+++ mouse.h
@@ -1,17 +1,26 @@
+// guards
 #ifndef MOUSE_H
 #define MOUSE_H
 
-#include "animal.h"
 #include <string>
+#include "animal.h"
 
-class mouse : public animal{
-	//*States*
-
+class mouse : public animal
+{
+	//*States*				// private, since each species of animal doesn't have a subclass
+							// mouse has no unique states
 	//*Behaviours*
-	public:
-		mouse();
-		std::string makeSound(int repeats);
-		void drawAnimal(int frame);
+	public:					
+		//*Constructors*
+		mouse();								// create a mouse
+
+		//*getters*
+		std::string makeSound(int repeats);	// make a mouse sound with number of repeats
+		void drawAnimal(int frame);			// draw the mouse, an integer represents which 'picture' to draw
+		
+		//*setters*
+		
+		//*destructors*
 		~mouse();
 };
 
