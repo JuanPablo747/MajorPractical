5d15f0190464ba7198008ffe8fd26891d44d4d31
ArieBennett <43255209+ArieBennett@users.noreply.github.com>
2018-10-11 10:54:10 +1030

    comments

--- dog.cpp
+++ dog.cpp
@@ -7,19 +7,27 @@ using namespace std;
 
 // ------ // constructors & destructors // ------ //
 
-dog::dog() : animal(){
+dog::dog() : animal()
+{
 	initial = 'd';
 	species = "Dog";
 	eats = "cat";
 }
 
-dog::~dog(){};
+dog::~dog()
+{
+	// nothing to be added
+};
 
 // ------ // ------ // getters // ------ // ------ //
 
-string dog::makeSound(int repeats){
+string dog::makeSound(int repeats)
+{
+	// initialize output - will give no string for 0 repeats
 	string bark = "";
-	for(int i=0;i<repeats;i++){
+	// extend bark string with animal's sound once per repeat
+	for(int i = 0; i < repeats; i++)
+	{
 		bark = bark + "Woof";
 		// if going for another loop, add a space
 		if(i+1 != repeats)
@@ -30,8 +38,9 @@ string dog::makeSound(int repeats){
 	return bark;
 }
 
-void dog::drawAnimal(int frame){
-	if(frame == 1)
+void dog::drawAnimal(int frame)
+{
+	if(frame == 1)					// if frame 1, draw animal
 	{
 		cout << "    ╱▏      ▕╲▕╲" << endl;
 		cout << "    ▏▏      ▕▏▔▔╲" << endl;
@@ -43,7 +52,7 @@ void dog::drawAnimal(int frame){
 		cout << "     ┗┛┗┛   ┗┛┗┛" << endl;
 	}
 	
-	else if (frame == 2)
+	else if (frame == 2)			// if frame 2, draw animal + print its sound
 	{
 		cout << "    ╱▏      ▕╲▕╲" << endl;
 		cout << "    ▏▏      ▕▏▔▔╲" << endl;
