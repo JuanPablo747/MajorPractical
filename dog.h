9dbda9ebad3faaa8e78c6f9118582a00bcfcb037
ArieBennett <43255209+ArieBennett@users.noreply.github.com>
2018-10-11 10:39:38 +1030

    comments

--- dog.h
+++ dog.h
@@ -1,17 +1,26 @@
+// guards
 #ifndef DOG_H
 #define DOG_H
 
-#include "animal.h"
 #include <string>
+#include "animal.h"
 
-class dog : public animal{
-	//*States*
-	std::string eats;
+class dog : public animal
+{
+	//*States*				// private, since each species of animal doesn't have a subclass
+	std::string eats;		// animal the dog eats
 	//*Behaviours*
-	public:
-		dog();
-		std::string makeSound(int repeats);
-		void drawAnimal(int frame);
+	public:					
+		//*Constructors*
+		dog();								// create a dog
+
+		//*getters*
+		std::string makeSound(int repeats);	// make a dog sound with number of repeats
+		void drawAnimal(int frame);			// draw the dog, an integer represents which 'picture' to draw
+		
+		//*setters*
+		
+		//*destructors*
 		~dog();
 };
 
