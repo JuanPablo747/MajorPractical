fe2ba98ecb29d51df201bc0b0978efe50dc1191e
Ryan Carmody <ryancarmody@outlook.com>
2018-10-11 12:03:09 +1030

    adding to the intro

--- extraFunctions.cpp
+++ extraFunctions.cpp
@@ -44,7 +44,7 @@ void intro()
     cout << "Welcome to the Riverbank Riddle" << endl << endl;
 
     cout << "The Riddle:" << endl; 
-    cout << "A man has to take a dog, a cat, and a mouse across a river. He can only take one of the animals across at a time. If the dog is left alone with the cat, the dog will eat the cat. If the cat is left alone with the mouse, the cat will eat the mouse. How does he get the three animals across the river?" << endl << endl << endl;
+    cout << "A man has to take a dog, a cat, and a mouse across a river. He can only take one of the animals across at a time. If he takes the mouse, the dog will eat the cat. If he takes the dog, the cat will eat the mouse." << endl << endl << endl;
 
     cout << "continue: (y/n)" << endl;
     char answer2 = 'g';
@@ -59,9 +59,12 @@ void intro()
             answer2= 'y';
             break;
         }
-
     cin >> answer2;
     }
+
+    system("clear");
+    cout << "How does he get the three animals across the river?" << endl;
+    pause(5);
     system("clear");
 }
 
